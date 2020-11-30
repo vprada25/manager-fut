439a31871e4e949bd63352e8923a8cd03914b1c0
vprada25 <v.prada@udla.edu.co>
2020-11-30 11:59:17 -0500

    feat: add json players

--- src/store.js
+++ src/store.js
@@ -22,6 +22,26 @@ const initState = {
             id: 4,
             name: "Duvan Zapata",
             photo: "https://fcf.com.co/wp-content/uploads/2019/04/Duvan-Zapata-1-445x400.png"
+        },
+        {
+            id: 5,
+            name: "David Ospina",
+            photo: "https://fcf.com.co/wp-content/uploads/2019/05/David-Ospina-445x400.png"
+        },
+        {
+            id: 6,
+            name: "Santiago Arias",
+            photo: "https://e00-co-marca.uecdn.es/claro/assets/multimedia/imagenes/2019/06/10/15601698611887.jpg"
+        },
+        {
+            id: 7,
+            name: "Jeison Murillo",
+            photo: "https://www.laopinion.com.co/sites/default/files/styles/640x370/public/2015/07/07/imagen/murillo.jpg"
+        },
+        {
+            id: 8,
+            name: "Duvan Zapata",
+            photo: "https://fcf.com.co/wp-content/uploads/2019/04/Duvan-Zapata-1-445x400.png"
         }
 
 
